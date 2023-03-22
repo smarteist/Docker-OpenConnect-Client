e418a797ad304ff1f82734f2bf03ba87da99bebd
Ali Hosseini <30802876+smarteist@users.noreply.github.com>
2023-03-22 13:45:12 +0330

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+if [[ -z "${RETRY_TIMEOUT}" ]]; then
+  RETRY_TIMEOUT=30
+fi
+
 run () {
   # Start openconnect
   echo "-----------" $(date -u) "-----------" >> oclogs.log
   echo $PASSWORD | openconnect $SERVER --servercert=$CERT --user=$USER --passwd-on-stdin --interface=OCTUN >> oclogs.log
 }
-
-
-startOpenConnect
-
 
 until (run); do
   echo "openconnect exited. Restarting process in $RETRY_TIMEOUT seconds…" >&2
